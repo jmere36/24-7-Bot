53b64af10a1b73fe828c06c3d7c3386d75bed0b9
jmere36 <jmere36@icloud.com>
2018-02-14 20:34:17 +1000

    server greeting

--- bot.js
+++ bot.js
@@ -29,5 +29,15 @@ client.on('message', message => {
     	message.reply('rip');
   	}
 });
+
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
