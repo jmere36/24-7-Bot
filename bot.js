aa116c9713a27fd0434d4bfd7294494fb55d3582
jmere36 <jmere36@icloud.com>
2018-02-14 21:11:18 +1000

    greeting?

--- bot.js
+++ bot.js
@@ -1,35 +1,23 @@
+/*
+  A bot that welcomes new guild members when they join
+*/
 
+// Import the discord.js module
 const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = 'NDEyMTQwNjI5NzcwODk1Mzcw.DWWqqg.QOCds9Iuk2NRhuQ4txFQHV3yA3s';
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'ya') {
-    	message.reply('YEET');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'but can you do this?') {
-    	message.reply('ideed i can: https://www.tenor.co/TyZY.gif ');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'rip') {
-    	message.reply('rip');
-  	}
-});
-
+// Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find('name', 'member-log');
@@ -39,5 +27,5 @@ client.on('guildMemberAdd', member => {
   channel.send(`Welcome to the server, ${member}`);
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+// Log our bot in
+client.login(token);
